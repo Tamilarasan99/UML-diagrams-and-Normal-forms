999bf47f223b81ad6a790e81d133e0f82326653f
Tamilarasan99 <83861821+Tamilarasan99@users.noreply.github.com>
2021-05-30 18:21:56 +0530

    Add files via upload

---
+++ pg.js
@@ -0,0 +1,49 @@
+
+    /*for (let i = 1; i <= 50; i++) {
+        let flag = 0;
+    
+         for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+    
+        if (i > 1 && flag == 0) {
+            console.log(i);
+        }
+    }*/
+    /*let i,j,string="";
+    for(i = 0 ; i <=5; i++){
+        for(j=1; j<=i; j++){
+           string+="*";
+        }
+        string+="\n";
+
+    }
+    console.log(string);*/
+/*let ln = "";
+   for (let i = 1; i <= 5; i++) {
+
+       for (let j = 1; j <= 5 - i; j++) {
+           ln += " ";
+        }
+  
+       for (let k = 0; k < 2 * i - 1; k++) {
+           ln += "*";
+         }
+         ln += "\n";
+}
+console.log(ln);*/
+var i = 1;
+let count=0;
+/*for(let i=1; i<= 10 ; i++){
+    count += i;
+    console.log(count);
+}*/
+while(i<=10){
+    count += i;
+    console.log(count);
+i++
+    
+}
